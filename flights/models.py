fa4ba51fdf83b5a4e880e0b834f72df010e80d8f
unknown <satbuana@yahoo.com>
2024-11-05 09:44:15 +0800

    Use wrong valid flight function

--- flights/models.py
+++ flights/models.py
@@ -17,8 +17,7 @@ class Flight(models.Model):
     def __str__(self):
         return f"{self.id} : {self.origin} to {self.destination}"
 
-    def is_valid_flight(self):
-        # return self.origin != self.destination and self.duration >= 0
+    def is_valid_flight(self):        
         return self.origin != self.destination or self.duration >= 0
 
 
